a98760e01a15919f68c78dc06f1fb9ff9755202c
Yadnesh Kulkarni <ykulkar2@uncc.edu>
2022-12-28 01:39:58 -0500

    Created first window

--- Graphics-Lore/main.cpp
+++ Graphics-Lore/main.cpp
@@ -2,8 +2,62 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void resize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, 800, 600);
+}
+
+void render(GLFWwindow* window)
+{
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World";
+	// Initializing GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "The lore", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, resize);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		render(window);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
